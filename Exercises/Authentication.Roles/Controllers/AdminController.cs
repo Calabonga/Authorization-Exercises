47127f40af63f044fd6e3e211a277f01c0924df4
Calabonga <calabonga@outlook.com>
2020-04-27 15:31:40 +0700

    Project for video about roles added

---
+++ Exercises/Authentication.Roles/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Roles.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Administrator")]
+        public IActionResult Administrator()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "Manager")]
+        public IActionResult Manager()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,model.UserName),
+                new Claim(ClaimTypes.Role,"Administrator")
+            };
+            var claimIdentity = new ClaimsIdentity(claims, "Cookie");
+            var claimPrincipal = new ClaimsPrincipal(claimIdentity);
+            await HttpContext.SignInAsync("Cookie", claimPrincipal);
+
+            return Redirect(model.ReturnUrl);
+        }
+
+        public IActionResult LogOff()
+        {
+            HttpContext.SignOutAsync("Cookie");
+            return Redirect("/Home/Index");
+        }
+    }
+}
---
+++ Exercises/Authentication.Roles/Startup.cs
@@ -0,0 +1,57 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Authentication.Roles
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("Cookie")
+                .AddCookie("Cookie", config =>
+                {
+                    config.LoginPath = "/Admin/Login";
+                    config.AccessDeniedPath = "/Home/AccessDenied";
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Administrator", builder =>
+                {
+                    builder.RequireClaim(ClaimTypes.Role, "Administrator");
+                });
+
+                options.AddPolicy("Manager", builder =>
+                {
+                    builder.RequireAssertion(x => x.User.HasClaim(ClaimTypes.Role, "Manager")
+                                                  || x.User.HasClaim(ClaimTypes.Role, "Administrator"));
+                });
+
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
