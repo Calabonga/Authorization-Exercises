32ca4e8571765d9520f8e17decad0ae15479bc84
Calabonga <dev@calabonga.net>
2020-05-25 16:06:42 +0700

    swagger packages installed

--- Exercises/Authorization.Swagger/Startup.cs
+++ Exercises/Authorization.Swagger/Startup.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace Authorization.Swagger
 {
@@ -9,6 +11,17 @@ namespace Authorization.Swagger
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Description = "Demo Swagger API v1",
+                    Title = "Swagger with IdentityServer4",
+                    Version = "1.0.0"
+
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -18,6 +31,16 @@ namespace Authorization.Swagger
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger UI Demo");
+                options.DocumentTitle = "Title";
+                options.RoutePrefix = "docs";
+                options.DocExpansion(DocExpansion.List);
+
+            });
 
             app.UseRouting();
 
