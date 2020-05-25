bc011d787da66e6450339acfe8c044acde72f4e8
Calabonga <dev@calabonga.net>
2020-05-25 15:59:16 +0700

    new project and one controller created

---
+++ Exercises/Authorization.Swagger/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Calabonga.DemoClasses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Swagger.Controllers
+{
+    [Route("[controller]")]
+    public class ApiController: ControllerBase
+    {
+        private readonly List<Person> _people = People.GetPeople();
+
+        [Route("[action]")]
+        public IActionResult GetAll()
+        {
+            return Ok(_people);
+        }
+
+        [Route("[action]/{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var item = _people.FirstOrDefault(x => x.Id == id);
+            return Ok(item);
+        }
+    }
+}
